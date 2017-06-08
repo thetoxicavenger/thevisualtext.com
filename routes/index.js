173aeed378b22173e826cafa4a4fbf8445ce75ea
Mitchell Cravens <mitch@fjld.co>
2017-06-08 13:19:19 -0700

    homepage done for now.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const {join} = require('path')
+// Authentication module.
+var auth = require('http-auth');
+var basic = auth.basic({
+    realm: "User Area",
+    file: join(__dirname, '..', '.htpasswd')
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.all('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* Use htpasswd auth for protected path */
+router.all('/app/*', auth.connect(basic));
+
+router.get('/app/index', function(req, res, next) {
+  res.render('app/index', { title: 'Express' });
 });
 
 module.exports = router;
